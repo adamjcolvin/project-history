87040a03c43b8931b0b1d12a4b6eb8ab3e1e5e7e
Adam Colvin <adamcolv@icloud.com>
2024-10-02 13:50:58 +0100

    Add Comment model with a project and a user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_02_124641) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_02_124818) do
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.integer "project_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_comments_on_project_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "projects", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -30,4 +40,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_02_124641) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "comments", "projects"
+  add_foreign_key "comments", "users"
 end

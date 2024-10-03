6e2fc4b965450e6109c495db11cd9d2a11ffaf30
Adam Colvin <adamcolv@icloud.com>
2024-10-03 15:08:29 +0100

    Stream updates to project history items

--- app/models/project_history_item.rb
+++ app/models/project_history_item.rb
@@ -1,4 +1,16 @@
 class ProjectHistoryItem < ApplicationRecord
+  include ActionView::RecordIdentifier
   belongs_to :project
   belongs_to :historical, polymorphic: true
+
+  after_create_commit :stream_to_project
+
+  private
+
+    def stream_to_project
+      broadcast_replace_to project,
+        target: "#{dom_id(project)}_project_history_items",
+        partial: "projects/project_history_items",
+        locals: { project: project }
+    end
 end
